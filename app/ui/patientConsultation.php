b593c4620c1016712dde78526ae017ef19229cd8
VittorioWeiLong <54586663+VittorioWeiLong@users.noreply.github.com>
2020-03-06 15:29:15 +0800

    Add UI features

---
+++ app/ui/patientConsultation.php
@@ -0,0 +1,26 @@
+
+<html>
+<header>
+    <?php include 'imports/codeLinks.php';?>
+    <link rel = "stylesheet" type = "text/css" href = "imports/stylesheet.css" />
+</header>
+
+
+<body>
+<!-- Import navigation bar -->
+<?php include 'imports/patientNavbar.php';?>
+
+
+</br>
+
+<div class="container col-12">
+
+    <div class="row d-flex justify-content-center">
+
+    </div>
+
+</div>
+
+</body>
+
+</html>
---
+++ app/ui/signup.php
@@ -0,0 +1,51 @@
+<html>
+<header>
+    <?php include 'imports/codeLinks.php';?>
+    <link rel = "stylesheet" type = "text/css" href = "imports/stylesheet.css" />
+</header>
+
+
+<body>
+
+<div class="container">
+<div class="row d-flex justify-content-center">
+
+
+        <form class="col-12 justify-content-center">
+
+        <div class="text-center">
+            <img src="images/logo.png" height="200" width="300"> 
+        </div>
+
+            <div class>
+                <label for="username"> <h3> Username: </h3> </label>
+                <input type="text" class="form-control input-group-lg" id="username">
+            </div>
+
+            </br>
+
+            <div class>
+            <label for="password"> <h3> Password: </h3> </label>
+                <input type="password" class="form-control input-group-lg" id="password">
+            </div>
+
+            </br>
+
+            <div class="text-right">
+                <button type="button" class="btn btn-primary btn-lg" id="signup"> Sign Up </button>
+                    &nbsp;
+                    &nbsp;
+                <button type="button" class="btn btn-primary btn-lg" id="login"> Login </button>
+            </div>
+        </form>
+
+</div>
+</div>
+
+<script>    
+
+</script>
+
+</body>
+
+</html>
