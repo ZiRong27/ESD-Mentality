6e09450ec0839bd973ccffdebd6a748f0c37f86f
syafiqahmr <syafiqahmr.2018@sis.smu.edu.sg>
2020-03-18 22:32:37 +0800

    Update payment.py

--- app/payment/payment.py
+++ app/payment/payment.py
@@ -5,57 +5,162 @@ from flask_cors import CORS
 # pip install --upgrade stripe
 import stripe
 
-from os import environ
-
+import time
+import json
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:8889/esd_payment'
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:IloveESMandPaul!<3@esd.cemjatk2jkn2.ap-southeast-1.rds.amazonaws.com/esd_patient'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db = SQLAlchemy(app)
+CORS(app)
 
-# To save key into os environment variable
-pub_key = environ.get('pub_key')
-secret_key = environ.get('secret_key')
+class Payment(db.Model):
+    __tablename__ = 'payment'
+    patient_id = db.Column(db.String, nullable=False)
+    date = db.Column(db.Date, nullable = False)
+    payment_id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
 
-stripe.api_key = secret_key
+    # def __init__(self, patient_id, amount):
+    #     # sets the properties (of itself when created)
+    #     self.patient_id = patient_id
+    #     self.amount = amount
+  
+
+    def json(self):
+        dto = {
+            'patient_id': self.patient_id, 
+            'date' : self.date ,
+            'payment_id' : self.payment_id ,
+            'amount' : self.amount
+        }
+        return dto  
+
+
+pub_key = 'pk_test_RTVKG6eUSKaY6R0IJvaK2Yp900zQwhahx5'
+stripe.api_key = 'sk_test_Jv4rWn5PYDRId7XNFssqQTS600rcH2uAbV'
 
 @app.route('/')
 def index():
     return render_template('index.php', pub_key = pub_key)
 
-@app.route('/success')
-def success():
-    print (request.get_json())
+@app.route('/success/<string:session_id>')
+def success(session_id):
+
+    session_id = session_id.split('=')[1]
+
+    # Check if payment is confirmed
+    events = stripe.Event.list(
+    type='checkout.session.completed',
+    created={
+        # Check for events created in the last hour.
+        'gte': int(time.time() - 60 * 60 * 2),
+    },
+    )
+
+    for event in events.auto_paging_iter():
+        session = event['data']['object'] 
+        session = dict(session) # convert stripe session class to dictionary
+        if session['id'] == session_id:
+            appointment_info = session['metadata']
+            amount = session['display_items'][0]['amount']
+
+    # add_to_transaction_history(amount, appointment_info)
+    # add_appointment()
+
+    # add_to_transaction_history
+    data = {
+        'patient_id': appointment_info['patient_id'],
+        'amount': float(amount)
+    }
+
+    payment = Payment(**data)
+
+    try:
+        print(db.session.add(payment))
+        print(db.session.commit())
+
+    except:
+        return jsonify({"message": "An error occured creating payment"}), 500
+
     return render_template('success.html', pub_key = pub_key)
 
+# def add_to_transaction_history(amount, appointment_info):
 
-# def payment_intent():
-#     intent = stripe.PaymentIntent.create(
-#         amount=1099,
-#         currency='sgd',
-#         payment_method_types=["card"],
-#         # Verify your integration in this guide by including this parameter
-#         metadata={'integration_check': 'accept_a_payment'},)
+#     data = {
+#         'patient_id': appointment_info['patient_id'],
+#         'amount': float(amount)
+#     }
 
-# Expected JSON for POST
-# 'line_items' [{'name', 'amount', 'amount', 'currency'}]
+#     payment = Payment(**data)
+
+#     try:
+#         db.session.add(payment)
+#         db.session.commit()
+
+#     except:
+#         return jsonify({"message": "An error occured creating payment"}), 500
+
+
+# This function generate a line item for checkout to the appropriate format
+# Compulsory parameters: amount, currency, name, quantity
+# Full details here:
+# https://stripe.com/docs/api/checkout/sessions/create#create_checkout_session-line_items
+def generate_line_item(doctor_id, time, date, price):
+    name = "Appointment Booking"
+    amount = price
+    currency = "sgd"
+    quantity = 1
+    description = time + " on " + date
+
+    line_items = [{
+        "name": name,
+        "amount": amount,
+        "currency": "sgd",
+        "quantity": quantity,
+        "description": description
+    }]
+    
+    return line_items
+
 
 @app.route('/checkout', methods=['POST'])
 def checkout():
     # check data posted
     try:
         data = request.get_json()  
+
         patient_id = data['patient_id']
-        line_items = data['line_items']
+        doctor_id = data['doctor_id']
+        time = data['time']
+        date = data['date']
+        price = data['price'] * 100 # Stripe calculate in cents
+
+        # create metadata for later use
+        appointment_info = {
+            "patient_id": patient_id,
+            "doctor_id": doctor_id,
+            "time": time,
+            "date": date
+        }
+
+        line_items = generate_line_item(doctor_id, time, date, price)
+        print(line_items)
 
         session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=line_items,
-            success_url='http://127.0.0.1:5000/success?session_id={CHECKOUT_SESSION_ID}?patient_id={patient_id}',
+            success_url='http://127.0.0.1:5005/success/session_id={CHECKOUT_SESSION_ID}',
             cancel_url='https://example.com/cancel',
+            metadata = appointment_info
         )
 
         checkout_session_id = session.id
-        return render_template('checkout.html', pub_key = pub_key,CHECKOUT_SESSION_ID = checkout_session_id, patient_id=patient_id)
+        return render_template('checkout.html', pub_key = pub_key,CHECKOUT_SESSION_ID = checkout_session_id, appointment_info = doctor_id)
     except KeyError:
         return jsonify({"message": "Invalid data parsed. Check required data needed."}), 400
     except stripe.error.InvalidRequestError:
