4fa85a4ab4b41cbcc6464ebf6530aedde073aaf3
syafiqahmr <syafiqahmr.2018@sis.smu.edu.sg>
2020-03-29 14:00:59 +0800

    Fix append in viewDoctor

--- app/ui/patient/viewDoctor.php
+++ app/ui/patient/viewDoctor.php
@@ -193,11 +193,13 @@ require_once '../include/protect.php';
                             '<input type="hidden" id="cost" value="' + price + '" />';
                     $('#bookForm').append(hidden_input);
 
+                    $('#timeslot-header').empty();
                     $('#timeslot-header').append("Select Your Prefered Timeslot");
 
                     timeslots_display = ['09:00 AM - 10:00 AM','10:00 AM - 11:00 AM','11:00 AM - 12:00 PM','12:00 PM - 13:00 PM','13:00 PM - 14:00 PM','14:00 PM - 15:00 PM','15:00 PM - 16:00 PM','16:00 PM - 17:00 PM','17:00 PM - 18:00 PM']
                     timeslots = ['09:00 AM','10:00 AM','11:00 AM','12:00 PM','13:00 PM','14:00 PM','15:00 PM','16:00 PM','17:00 PM']
-
+                    
+                    $('#timeslotTable').empty(); 
                     for (i = 0; i < timeslots.length; i++){
                         if (jQuery.inArray(timeslots[i], timings) == -1){ // if timing is available
                             Row = 
