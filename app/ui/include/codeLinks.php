5526fcf85849ce65818ff3eb0d41a4680a9764b9
VittorioWeiLong <54586663+VittorioWeiLong@users.noreply.github.com>
2020-03-29 12:00:56 +0800

    Update codeLinks.php

--- app/ui/include/codeLinks.php
+++ app/ui/include/codeLinks.php
@@ -30,15 +30,15 @@
     <script>
       //Sets the IP addresses of ALL microservices here!!
       //All these are working! Uncomment to try them, you will NOT need to run any of these microservices yourself
-      //patientip = "54.254.155.190:5001";
-      //doctorip = "13.229.63.17:5002";
-      //appointmentip = "13.250.25.187:5003";
-      //consultationip = "18.141.146.207:5004";
+      patientip = "54.255.192.51:5001";
+      doctorip = "13.229.63.17:5002";
+      appointmentip = "13.250.25.187:5003";
+      consultationip = "18.141.146.207:5004";
       paymentip = "127.0.0.1:5005";
-      patientip = "127.0.0.1:5001";
-      doctorip = "127.0.0.1:5002";
-      appointmentip = "127.0.0.1:5003";
-      consultationip = "127.0.0.1:5004";
+      //patientip = "127.0.0.1:5001";
+      //doctorip = "127.0.0.1:5002";
+      //appointmentip = "127.0.0.1:5003";
+      //consultationip = "127.0.0.1:5004";
     </script>
 
     <!-- tempus dominus bootstrap 4 https://tempusdominus.github.io/bootstrap-4/ -->
