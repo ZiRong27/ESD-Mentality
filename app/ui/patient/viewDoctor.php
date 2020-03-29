aacd10fe9ffe649cf5e93b513c2e2c26fb6dd5b8
ZiRong27 <zirong.tan.2018@sis.smu.edu.sg>
2020-03-29 16:28:21 +0800

    Update viewDoctor.php
    spelling error

--- app/ui/patient/viewDoctor.php
+++ app/ui/patient/viewDoctor.php
@@ -195,7 +195,7 @@ require_once '../include/protect.php';
                     $('#bookForm').append(hidden_input);
 
                     $('#timeslot-header').empty();
-                    $('#timeslot-header').append("Select Your Prefered Timeslot");
+                    $('#timeslot-header').append("Select Your Preferred Timeslot");
 
                     timeslots_display = ['09:00 AM - 10:00 AM','10:00 AM - 11:00 AM','11:00 AM - 12:00 PM','12:00 PM - 13:00 PM','13:00 PM - 14:00 PM','14:00 PM - 15:00 PM','15:00 PM - 16:00 PM','16:00 PM - 17:00 PM','17:00 PM - 18:00 PM']
                     timeslots = ['09:00 AM','10:00 AM','11:00 AM','12:00 PM','13:00 PM','14:00 PM','15:00 PM','16:00 PM','17:00 PM']
